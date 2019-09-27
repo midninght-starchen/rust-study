b143e7030f05ca5957f8e1567fa40ab221440f58
midnight-starchen <963307255@qq.com>
2019-09-27 21:11:42 +0800

    happy

---
+++ three_day/three_day.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("the value of y is: {}", y);
+
+    let lbj = (524, 82.63, 32);
+
+    let (a, b, c) = lbj;
+
+    println!("all of tnem: {}, {}", lbj.0, lbj.2);
+
+
+    let a = [1, 2, 3, 4, 5];
+    println!("{:?} {:?}", a[2], a[3]);
+
+
+    let b: [i32; 4] = [1, 2, 3, 4];
+    println!("{}", b[3]);
+
+
+
+    fn kjk(){
+    }
+    kjk();
+    
+}
